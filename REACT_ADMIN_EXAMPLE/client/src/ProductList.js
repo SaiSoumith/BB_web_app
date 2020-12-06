ead8a22af96e5924839f44dc0b93d791c5160725
SaiSoumith <mennenisoumith2@gmail.com>
2020-12-06 15:15:47 +0530

    with all fetch and axios errors

--- REACT_ADMIN_EXAMPLE/client/src/ProductList.js
+++ REACT_ADMIN_EXAMPLE/client/src/ProductList.js
@@ -1,13 +1,25 @@
 
 import React, { useEffect, useState } from 'react';
 import { Button, Table } from 'react-bootstrap';
-import {useGlobalFilter} from 'react-table';
+//import {useGlobalFilter} from 'react-table';
+import axios from 'axios';
+
+
+const apiUrl = 'getTrucksList';
+//const accessToken='5vp9lltyyt8qjlehphrta2ys8f88pr12';
+// const apiUrl='http://localhost:3000/products';
+// const authAxios=axios.create({
+//   baseURL:apiUrl
+  // headers:{
+  //   Authorization:`Bearer ${accessToken}`
+  // }
+// })
 
 
 function  ProductList(props) {
 
 
-const [error,setError]=useState(null);
+//const [error,setError]=useState(null);
 const  [products,setProducts]=useState([]);
 const [search,setSearch]=useState('');
   
@@ -15,42 +27,83 @@ const [filteredProducts,setFilteredProducts]=useState([]);
 
 
   useEffect(()=>{
-
-  loadData();
-
+    console.log("hi .");
+ loadData();
+  
   },[]);
 
 
 
   const loadData=()=>{
 
-   
-    const apiUrl = ' http://localhost:3000/products';
 
-      fetch(apiUrl)
-        .then(res => res.json())
-        .then(
-          (result) => {
-            setProducts(result);
-          },
-          (error) => {
-            setError(error);
-          }
-        )
+
+    const requestOptions = {
+      
+      // credentials:'include',
+      headers: { 
+          'Content-Type': 'application/json',
+          'Authorization': 'Token : 5vp9lltyyt8qjlehphrta2ys8f88pr12'
+          
+        
+      }
+  };
+
+
+
+  axios.post('https://preprod-boss-auth.blackbuck.com/fms/api/dashboard/truck/getTrucksList', {"page":1,"size":10},requestOptions)
+
+    // fetch('https://preprod-boss-auth.blackbuck.com/fms/api/dashboard/truck/getTrucksList',{
+    //   method: 'POST',
+     
+    //   headers:{
+        
+    //     // 'Host': 'preprod-boss-auth.blackbuck.com',
+    //     // 'authority': 'preprod-boss-auth.blackbuck.com',
+    //     // 'accept': 'application/json',
+    //     'Authorization': 'Token :5vp9lltyyt8qjlehphrta2ys8f88pr12',
+    //     'Content-Type': 'application/json',
+    //    // 'origin': 'https://sitemployees.blackbuck.com',
+    //      //'sec-fetch-mode': 'no-cors',
+    //     //'referer': 'https://sitemployees.blackbuck.com/',
+    //     // 'sec-fetch-site': 'same-site',
+    //     // 'Content-Length': '20'
+    //   },
+    //   body: {"page":1,"size":10}
+  
+  
+  
+    // })
+      .then((res) =>{ 
+        console.log(res);
+        return res.json()})
+      .then(
+        (result) => {
+          console.log(result.data);
+          setProducts(result.data);
+          
+        }
+      )
+  // let result=authAxios.get(`products`);
+  //  setProducts(result.data);
+  //  console.log(result.data);
+  //  setError(err.message);
+
+    
   }
 
 
-   useEffect(()=>{
+  //  useEffect(()=>{
  
-    setFilteredProducts(
-      products.filter((product) =>
-      product.truck_id.includes(search)
-    ));
-    setFilteredProducts(
-      products.filter((product) =>
-      product.document_status.toLowerCase().includes(search.toLowerCase())
-    ));
-   },[search,products]);
+  //   setFilteredProducts(
+  //     products.filter((product) =>
+  //     product.truck_id.includes(search)
+  //   ));
+  //   setFilteredProducts(
+  //     products.filter((product) =>
+  //     product.document_status.toLowerCase().includes(search.toLowerCase())
+  //   ));
+  //  },[search,products]);
 
 
 
@@ -62,18 +115,27 @@ const [filteredProducts,setFilteredProducts]=useState([]);
           <Table>
             <thead>
               <tr>
-                <th>Truck Id</th>
-                <th>Document status</th>
+                <th>Truck Number</th>
+                
+                <th>Carrier Mobile</th>
+                <th>Truck RC Status</th>
+                <th>Truck Owner KYC Status</th>
+                {/* <th>Document status</th> */}
                 {/* <th>SKU</th>
                 <th>Price</th> */}
+                <th>Created On</th>
                 <th>Action</th>
               </tr>
             </thead>
             <tbody>
-              {filteredProducts.map(product => (
+              {products.map(product => (
                 <tr key={product.id}>
-                  <td>{product.truck_id}</td>
-                  <td>{product.document_status}</td>
+                  <td>{product.truck_number}</td>
+                   <td>{product.carrier_mobile}</td>
+                   <td>{product.truck_kyc_status}</td>
+                   <td>{product.truck_owner_kyc_status}</td>
+                  
+                  <td>{product.created_on}</td>
 
                   {/* <td>{product.product_name}</td>
                   <td>{product.sku}</td>
